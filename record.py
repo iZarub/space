f7466657173c4c078801fac4363d123846272db1
mikhail-shmakov <mikhail_shmakov@mail.ru>
2020-11-02 13:15:19 +0300

    Обрахзца кода

---
+++ record.py
@@ -0,0 +1,15 @@
+import time
+import os
+
+canvas = None
+delay = None
+index = 0
+
+def dump():
+    pass
+
+def start(_canvas, _delay, _name):
+    pass
+
+def finish():
+    pass
---
+++ solar_model.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+# license: GPLv3
+
+gravitational_constant = 6.67408E-11
+"""Гравитационная постоянная Ньютона G"""
+
+
+def calculate_force(body, space_objects):
+    """Вычисляет силу, действующую на тело.
+
+    Параметры:
+
+    **body** — тело, для которого нужно вычислить дейстующую силу.
+
+    **space_objects** — список объектов, которые воздействуют на тело.
+    """
+
+    pass
+
+
+def move_space_object(body, dt):
+    """Перемещает тело в соответствии с действующей на него силой.
+
+    Параметры:
+
+    **body** — тело, которое нужно переместить.
+    """
+
+    pass
+
+
+def recalculate_space_objects_positions(space_objects, dt):
+    """Пересчитывает координаты объектов.
+
+    Параметры:
+
+    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+
+    **dt** — шаг по времени
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
